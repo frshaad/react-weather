022e3acdfbc047fc1997b06f3dfd16b090a75981
Farshad Hatami <farshad.hatami95@gmail.com>
2024-02-28 02:13:04 +0330

    add shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from './components/ui/button'
+
 export default function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
 }
