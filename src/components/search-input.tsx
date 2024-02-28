2a8798cc36f88692098aa67b64792b114ec891ce
Farshad Hatami <farshad.hatami95@gmail.com>
2024-02-28 22:22:57 +0330

    add debounced value to search input

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,4 +1,5 @@
 import { TiLocationArrow } from 'react-icons/ti'
+import { useDebounceValue } from 'usehooks-ts'
 
 import {
   Tooltip,
@@ -11,9 +12,18 @@ import { Button } from './ui/button'
 import { Input } from './ui/input'
 
 export default function SearchInput() {
+  const [searchQuery, setValue] = useDebounceValue('', 500)
+
+  console.log(searchQuery)
+
   return (
     <div className="flex w-full max-w-sm items-center space-x-2">
-      <Input type="text" placeholder="Search City" />
+      <Input
+        type="text"
+        placeholder="Search City"
+        defaultValue=""
+        onChange={e => setValue(e.target.value)}
+      />
       <TooltipProvider>
         <Tooltip>
           <TooltipTrigger asChild>
