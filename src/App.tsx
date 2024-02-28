3f87d5736f41360bfd94a2f566dab2b1263676be
Farshad Hatami <farshad.hatami95@gmail.com>
2024-02-28 22:06:08 +0330

    add search input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import { Link } from 'react-router-dom'
-
-import { Button } from './components/ui/button'
+import SearchInput from './components/search-input'
 
 export default function App() {
   return (
-    <div>
-      <h1>Lorem ipsum dolor sit.</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur ipsa
-        minus, dicta blanditiis sunt fugiat molestiae corrupti! Neque nostrum
-        perferendis accusantium blanditiis a aspernatur iusto. Cumque, tempore
-        reiciendis. Quisquam odit cumque accusantium cum molestiae ipsam minus,
-        explicabo sequi repellendus perferendis nisi eveniet consequatur et
-        atque tempore quae saepe distinctio tenetur.
-      </p>
-      <Button>Click me</Button>
-      <Link to="/settings">Settings</Link>
+    <div className="mx-auto mt-10 flex max-w-lg flex-col justify-center gap-8">
+      <SearchInput />
     </div>
   )
 }
--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,3 +1,31 @@
+import { TiLocationArrow } from 'react-icons/ti'
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+
 export default function SearchInput() {
-  return <div>SearchInput</div>
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="text" placeholder="Search City" />
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="outline" size="icon" type="submit">
+              <TiLocationArrow size={22} />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Use your current location</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  )
 }
