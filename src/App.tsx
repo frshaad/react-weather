3a1167ed76c20662e82e446173792655f0f25013
Farshad Hatami <farshad.hatami95@gmail.com>
2024-02-28 12:10:34 +0330

    add css system font stack

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ import { Button } from './components/ui/button'
 export default function App() {
   return (
     <div>
+      <h1>Lorem ipsum dolor sit.</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur ipsa
+        minus, dicta blanditiis sunt fugiat molestiae corrupti! Neque nostrum
+        perferendis accusantium blanditiis a aspernatur iusto. Cumque, tempore
+        reiciendis. Quisquam odit cumque accusantium cum molestiae ipsam minus,
+        explicabo sequi repellendus perferendis nisi eveniet consequatur et
+        atque tempore quae saepe distinctio tenetur.
+      </p>
       <Button>Click me</Button>
     </div>
   )
