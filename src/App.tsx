88fd35013b2c4a9704ff78256d53d0c29d737033
Farshad Hatami <farshad.hatami95@gmail.com>
2024-03-04 15:04:24 +0330

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import { lazy } from 'react'
 
-import { useFetchForecast } from '@/hooks/useFetchForecast'
-
 const UnitToggle = lazy(() => import('@/app/features/unit/toggle-unit'))
 const ModeToggle = lazy(() => import('@/components/mode-toggle'))
 const SearchInput = lazy(() => import('@/components/search-input'))
 
 export default function App() {
-  const { data: forecastWeather, isLoading } = useFetchForecast(
-    36.63296126999999,
-    52.27092741999998
-  )
-
-  if (!isLoading && !!forecastWeather) {
-    console.log(forecastWeather)
-  }
-
   return (
     <div className="mx-auto mt-10 flex max-w-2xl">
       <SearchInput />
