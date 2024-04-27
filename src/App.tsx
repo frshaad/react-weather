8ab4a7939f122aa3e105b8165d08c15e0521620d
Farshad Hatami <farshad.hatami95@gmail.com>
2024-04-27 17:53:34 +0330

    add redirect to tehran as default page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import ModeToggle from '@/components/mode-toggle';
 import SearchInput from '@/components/search-input';
 
 export default function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/35.6892523,51.3896004'); // navigate to Tehran weather page
+  }, [navigate]);
+
   return (
     <div className="container max-w-5xl space-y-5 pt-5">
       <div className="flex">
