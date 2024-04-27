d8ae6ec8f56fb4ce268b2bbcc1db44995b896158
Farshad Hatami <farshad.hatami95@gmail.com>
2024-04-27 17:05:08 +0330

    remove loading text fomr home page

--- src/pages/location.tsx
+++ src/pages/location.tsx
@@ -1,16 +1,7 @@
-import { useParams } from 'react-router-dom';
-
 import CurrentOverview from '@/components/current-overview';
 import ForecastOverview from '@/components/forecast-overview';
-import { LocationParam } from '@/types';
 
 export default function Location() {
-  const { locationCoords } = useParams<LocationParam>();
-
-  if (!locationCoords) {
-    return <h2>Loading...</h2>;
-  }
-
   return (
     <section className="space-y-12">
       <CurrentOverview />
